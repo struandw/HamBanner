c00100cb71e0876f7288c674760ad334a81f354c
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2022-04-01 22:21:02 +0100

    fuckit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import karelia
 import sys
 import time
+import pprint
 
-hambanner = karelia.bot("Hambanner", "xkcd")
+hambanner = karelia.bot("HamBanner", "xkcd")
 
 hambanner.stock_responses["short_help"] = "I respond to spam."
 hambanner.stock_responses[
@@ -12,16 +13,21 @@ hambanner.stock_responses[
 hambanner.connect()
 message_lengths = {}
 
-minimum_unpenalised_length = 40
-warning_threshold = 100
+minimum_unpenalised_length = 30
+warning_threshold = 400
 time_step = 15
-dec_multiplier = 0.9
+dec_multiplier = 0.85
+
+warning_cooldown = 120
+most_recent_warnings = {}
 
 last_dec_time = time.time()
+backoff = 1
 
 while True:
     try:
         while True:
+            backoff = 1 
             message = hambanner.parse()
             if message.type == "send-event":
                 if message.data.sender.id not in message_lengths:
@@ -31,16 +37,22 @@ while True:
                 ] += minimum_unpenalised_length - len(message.data.content)
 
                 if message_lengths[message.data.sender.id] > warning_threshold:
-                    hambanner.reply("You're sending spam. Please don't do that.")
+                    if message.data.sender.id not in most_recent_warnings:
+                        most_recent_warnings[message.data.sender.id] = 0
+                    if most_recent_warnings[message.data.sender.id] < time.time() - warning_cooldown:
+                        hambanner.reply("You're sending lots of short messages. Please consider consolidating them into fewer, longer ones.")
+                        message_lengths[user_id] = warning_threshold
+                        most_recent_warnings[message.data.sender.id] = time.time()
+                        with open('count.txt', 'r') as f:
+                            count = int(f.read())
+                        with open('count.txt', 'w') as f:
+                            f.write(str(count + 1))
 
             if time.time() > last_dec_time + time_step:
                 for user_id in message_lengths:
                     message_lengths[user_id] *= dec_multiplier
                 last_dec_time = time.time()
-
-    except SystemExit:
-        # Catches !kill commands to the bot and ctrl+c/equivalent
-        sys.exit(0)
     finally:
-        time.sleep(1)
+        time.sleep(backoff)
         hambanner.connect()
+        backoff *= 2
---
+++ testing.py
@@ -0,0 +1,97 @@
+import karelia
+import sys
+import time
+import pprint
+
+hambanner = karelia.bot("HamBannerTweaker", "test")
+
+hambanner.stock_responses["short_help"] = "I respond to spam."
+hambanner.stock_responses[
+    "long_help"
+] = "I respond to spam. Specifically, when users are percieved to sending repeated low-quality messages in a short window, I warn. Direct criticism to @PouncySilverkitten and the cabal."
+
+hambanner.connect()
+message_lengths = {}
+
+minimum_unpenalised_length = 30
+warning_threshold = 400
+time_step = 15
+dec_multiplier = 0.85
+
+warning_cooldown = 120
+most_recent_warnings = {}
+
+last_dec_time = time.time()
+backoff = 1
+
+while True:
+    try:
+        while True:
+            backoff = 1 
+            message = hambanner.parse()
+            if message.type == "send-event":
+                if message.data.sender.id not in message_lengths:
+                    message_lengths[message.data.sender.id] = 0
+                message_lengths[
+                    message.data.sender.id
+                ] += minimum_unpenalised_length - len(message.data.content)
+
+                if message_lengths[message.data.sender.id] > warning_threshold:
+                    if message.data.sender.id not in most_recent_warnings:
+                        most_recent_warnings[message.data.sender.id] = 0
+                    if most_recent_warnings[message.data.sender.id] < time.time() - warning_cooldown:
+                        hambanner.reply("You're sending lots of short messages. Please consider consolidating them into fewer, longer ones.")
+                        message_lengths[user_id] = warning_threshold
+                        most_recent_warnings[message.data.sender.id] = time.time()
+                        with open('count.txt', 'r') as f:
+                            count = int(f.read())
+                        with open('count.txt', 'w') as f:
+                            f.write(str(count + 1))
+
+            if time.time() > last_dec_time + time_step:
+                for user_id in message_lengths:
+                    message_lengths[user_id] *= dec_multiplier
+                last_dec_time = time.time()
+
+            
+            if message.type == "send-event":
+                if message.data.content.startswith('!minlength '):
+                    try:
+                        minimum_unpenalised_length = int(message.data.content.split()[1])
+                        hambanner.reply(f"Minimum length set to {minimum_unpenalised_length}")
+                    except:
+                        hambanner.reply("Invalid minimum length.")
+                if message.data.content.startswith('!threshold '):
+                    try:
+                        warning_threshold = int(message.data.content.split()[1])
+                        hambanner.reply(f"Threshold set to {warning_threshold}")
+                    except:
+                        hambanner.reply("Invalid threshold.")
+                if message.data.content.startswith('!time '):
+                    try:
+                        time_step = int(message.data.content.split()[1])
+                        hambanner.reply(f"Time step set to {time_step}")
+                    except:
+                        hambanner.reply("Invalid time step.")
+                if message.data.content.startswith('!dec_multiplier '):
+                    try:
+                        dec_multiplier = float(message.data.content.split()[1])
+                        hambanner.reply(f"Decay multiplier set to {dec_multiplier}")
+                    except:
+                        hambanner.reply("Invalid dec_multiplier.")
+                if message.data.content.startswith('!cooldown '):
+                    try:
+                        warning_cooldown = int(message.data.content.split()[1])
+                        hambanner.reply(f"Cooldown set to {warning_cooldown}")
+                    except:
+                        hambanner.reply("Invalid cooldown.")
+                if message.data.content == "!tabularasa":
+                    message_lengths = {}
+                    hambanner.reply("Slate cleaned.")
+                if message.data.content == '!params':
+                    hambanner.reply(f"minimum_unpenalised_length = {minimum_unpenalised_length}\nwarning_threshold = {warning_threshold}\ntime_step = {time_step}\ndec_multiplier = {dec_multiplier}\nwarning_cooldown = {warning_cooldown}")
+
+    finally:
+        time.sleep(backoff)
+        hambanner.connect()
+        backoff *= 2
